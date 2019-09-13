babca916e26638e386c018ca9866eef7dae8ce94
axeldong <axeldong3@gmail.com>
2019-09-13 11:18:16 -0400

    final

--- scripts/ActionServer.py
+++ scripts/ActionServer.py
@@ -21,18 +21,17 @@ def get_radian(current_pose):
     return theta_radian
 
 
-#in this method we will use euler_from_quaternion function to get the radiance 
-def is_get_goal(a,b,c):
-    print "current radian is: " + str(get_radian(a))
-    print "start radian is: " + str(get_radian(b))
-    print "goal_radiance: " + str(c.angular.z)
+#in this method we will use euler_from_quaternion function to get the radiance
+def get_target_radiance(a,b,c):
     #get the target radian
     target_angle_radian = (c.angular.z + get_radian(b)) % (2 * math.pi)
     if(target_angle_radian > math.pi):
         target_angle_radian = target_angle_radian - 2 *math.pi 
-    print "target radian is" + str(target_angle_radian)
+    return target_angle_radian
 
+    
     current_radian = get_radian(a)
+
     if(abs(current_radian - target_angle_radian) > 0.1):
         return False
     else:
@@ -46,34 +45,64 @@ def get_twist(current_pose, start_pose):
 def do_action(goal):
     global current_pose
 
+    feedback = ActionServerFeedback()
+    feedback.angle_changed = Twist()
+    radiance_to_change = goal.angle_to_change.angular.z
+
+    speed = Twist()
+    speed.angular.z = 0.1
+    time_duration = radiance_to_change / speed.angular.z
+
     start_time = time.time()
     start_pose = current_pose
     update_count = 0
-    speed = Twist()
-    speed.angular.z = 0.1
+    
     pub.publish(speed)
-    while not is_get_goal(current_pose, start_pose, goal.angle_to_change):
+
+    target_angle_radian = get_target_radiance(current_pose, start_pose, goal.angle_to_change)
+
+    while (time.time() - start_time) < time_duration:#not is_get_goal(current_pose, start_pose, goal.angle_to_change):
         if server.is_preempt_requested():
             result = ActionServerResult()
             result.angle_changed = get_twist(current_pose, start_pose)
             result.updates_sent = update_count
             server.set_preempted(result, "ActionServer preempted")
 
-        feedback = ActionServerFeedback()
-        feedback.angle_changed = Twist()
-        feedback.angle_changed.angular.z = 0.1 * (time.time() - start_time)#get_twist(current_pose, start_pose)
+        
+        feedback.angle_changed.angular.z = speed.angular.z * (time.time() - start_time)#get_twist(current_pose, start_pose)
 
         server.publish_feedback(feedback)
         update_count += 1
-
+        print "current radian is: " + str(get_radian(current_pose))
+        print "start radian is: " + str(get_radian(start_pose))
+        print "goal_radiance: " + str(goal.angle_to_change.angular.z)
+        print "target radian is" + str(target_angle_radian)
         time.sleep(1.0)
+    
+    while abs(get_radian(current_pose) - target_angle_radian) > 0.1:
+        if server.is_preempt_requested():
+            result = ActionServerResult()
+            result.angle_changed = get_twist(current_pose, start_pose)
+            result.updates_sent = update_count
+            server.set_preempted(result, "ActionServer preempted")
+
         
+        feedback.angle_changed.angular.z = speed.angular.z * (time.time() - start_time)#get_twist(current_pose, start_pose)
+
+        server.publish_feedback(feedback)
+        update_count += 1
+        print "current radian is: " + str(get_radian(current_pose))
+        print "start radian is: " + str(get_radian(start_pose))
+        print "goal_radiance: " + str(goal.angle_to_change.angular.z)
+        print "target radian is" + str(target_angle_radian)
+        time.sleep(1.0)
+    
     speed.angular.z = 0.0
     print 'stop!!!!' + str(speed.angular.z)
     pub.publish(speed)
 
     result = result = ActionServerResult()
-    result.angle_changed = get_twist(current_pose, start_pose)
+    result.angle_changed.angular.z = 0.1 * (time.time() - start_time)
     result.updates_sent = update_count
     server.set_succeeded(result, "ActionServer completed successfully")
 
